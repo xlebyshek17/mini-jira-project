8f213b4470d3e4a98e04ce1824854e50e3c63515
xlebyshek17 <kristina1111113@gmail.com>
2026-01-18 21:17:49 +0100

    added cors to server

--- backend/server.js
+++ backend/server.js
@@ -4,19 +4,21 @@ const dotenv = require('dotenv');
 const authRoutes = require('./routes/authRoutes');
 const projectRoutes = require('./routes/projectRoutes');
 const taskRoutes = require('./routes/taskRoutes');
+const cors = require('cors');
 
-// 1. Ładowanie zmiennych z pliku .env
+// Ładowanie zmiennych z pliku .env
 dotenv.config();
 
 const app = express();
 
-// 2. Middleware do obsługi danych JSON (potrzebne do API)
 app.use(express.json());
+app.use(cors());
+
 app.use('/api/auth', authRoutes);
 app.use('/api/projects', projectRoutes);
 app.use('/api/tasks/', taskRoutes);
 
-// 3. Połączenie z MongoDB Atlas
+// Połączenie z MongoDB Atlas
 mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log('Sukces: Połączono z MongoDB Atlas!'))
   .catch((err) => {
@@ -24,13 +26,8 @@ mongoose.connect(process.env.MONGO_URI)
     console.error(err.message);
     process.exit(1); 
   });
-
-// 4. Prosta trasa (Route) do testu
-app.get('/', (req, res) => {
-  res.send('Serwer TaskFlow działa poprawnie!');
-});
-
-// 5. Uruchomienie serwera
+  
+// Uruchomienie serwera
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Serwer nasłuchuje na porcie ${PORT}`);
